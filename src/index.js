8185c1bf7c4c4a24c64b49bb553cdbc685131653
varunyk1805 <varunyk1805@gmail.com>
2022-01-13 15:56:36 +0200

    комент на рядок "import './js/form-login';"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/main.scss';
-import './js/form-login';
+// import './js/form-login';
 
 
 // DATA_URL = 'https://team-project-1da18-default-rtdb.europe-west1.firebasedatabase.app/user.json';  // Путь к данным пользователя в базе данных
