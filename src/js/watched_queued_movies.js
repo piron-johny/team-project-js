8e11d5045cf6607f9c42610ba4fb3e194ce639da
Vitalii-Dzoma <vitoos900@gmail.com>
2022-01-14 00:40:36 +0200

    precommit

--- src/js/watched_queued_movies.js
+++ src/js/watched_queued_movies.js
@@ -10,12 +10,12 @@ const Queued = [];
 
 async function fetchMovies() {
    const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=3c97a1babd597f31c1fa5b3567357dfb`)
-    const data = await response.json()
+    const data = await response.json();
     console.log(data.results)
-     
-    dataDiv.insertAdjacentHTML('afterbegin', moviesRender(data.results));
- getClickedElem()
-    return data.results;
+       dataDiv.insertAdjacentHTML('afterbegin', moviesRender(data.results));
+   
+    getClickedElem()
+    return data;
 }
 
 
@@ -30,9 +30,31 @@ fetchMovies(550)
 addArrayToLocalStorage([])
 
 function addMovieToWatched(e) {
-    e.preventDefault()
-    console.log(e.srcElement.parentElement.attributes.value.value)
+     e.preventDefault();
+    fetchMovies(550).then(data => {
+        
+      
+        data.results.forEach(elem => {
+            console.log(+e.srcElement.parentElement.attributes.value.value === elem.id);
+            if (+e.srcElement.parentElement.attributes.value.value === elem.id) {
+                try {
+                    const savedData = localStorage.getItem('Watched')
+                    const parsedData = JSON.parse(savedData)
+                    console.log(parsedData)
+                    parsedData.push(elem)
+                    localStorage.setItem('Watched', JSON.stringify(parsedData))
+                    
+                    
+                } catch (error) {
+                    console.log(error)
+                }
+        
+            }
+        })
     
+    })
+    
+     
 }
 
 
